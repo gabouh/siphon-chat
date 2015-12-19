8a718bf597c41982e3881bbe2cf0f29f87af7694
James Potter <james.potter@gmail.com>
2015-12-19 19:04:55 +0700

    Copied in the current app source.

---
+++ index.ios.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Text,
+  TextInput,
+  ScrollView,
+  View
+} = React;
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      messages: []
+    }
+  },
+  componentDidMount: function() {
+    this.ws = new WebSocket('ws://siphon-chat.herokuapp.com');
+    this.ws.onmessage = function(event) {
+      if (event.data != 'ping') {
+        this.setState({
+          messages: [event.data].concat(this.state.messages)
+        });
+      }
+    }.bind(this);
+    this.ws.onerror = function() {
+      console.log('WebSocket error: ', arguments);
+    };
+  },
+  componentWillUnmount: function() {
+    this.ws.close();
+  },
+  handleSubmit: function(event) {
+    console.log('Sending: ' + event.nativeEvent.text);
+    this.ws.send(event.nativeEvent.text);
+    this.refs.textInput.setNativeProps({text: ''});
+  },
+  render: function() {
+    return (
+      <View style={{paddingTop: 20}}>
+        <TextInput
+          ref='textInput'
+          autoCapitalize='none'
+          autoCorrect='false'
+          placeholder='Enter a chat message...'
+          returnKeyType='send'
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 10}}
+          onSubmitEditing={this.handleSubmit.bind(this)}
+        />
+        <ScrollView style={{height: 400}}>
+          {
+            this.state.messages.map(m => {
+              return <Text style={{margin: 10}}>{m}</Text>
+            })
+          }
+        </ScrollView>
+      </View>
+    );
+  }
+});
+
+AppRegistry.registerComponent('App', () => App);
